585a7da7e5dea47e8ed1ed4f2bdd11b99a803b0a
Samuel Murray <smurra28@uncc.edu>
2019-09-30 11:44:09 -0400

    Added /templates endpoint

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
+const express = require('express')
+const bodyParser = require('body-parser');
 const sgMail = require('@sendgrid/mail');
+
+const app = express()
+const port = 3000;
+//  Pulls in API key from env variable source the file from terminal.
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'smurra28@uncc.edu',
-  from: 'samuela.y.murray@gmail.com',
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg);
+
+
+app.use(bodyParser.json());
+
+// Perhaps with a nodejs templating engine down the line we can do /templates/:templateName
+app.post('/templates', (req, res) => {
+    const msg = {
+        to: req.body.recipient,
+        from: req.body.sender,
+        subject: req.body.recipient,
+        /* Possible template IDs at the moment:
+         * d-2f299cb6611e4054ab4ced10df8f9af2 Template01: One column
+         * d-a624e35c613740328678d97b6eeb829b Template02: Two Column
+         */
+        templateId: req.body.templateID,
+        dynamic_template_data: {
+          username: req.body.username,
+          body: req.body.body,
+          url: req.body.url
+        },
+      };
+
+      sgMail.send(msg).then(() => {
+        return res.send('Email has been sent!');
+      }).catch((error) => {
+        res.status(500)
+        //oops!
+        return res.send(error);
+      })
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
